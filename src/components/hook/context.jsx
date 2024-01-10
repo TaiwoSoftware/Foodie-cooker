29f2b3d89dca778af41c9abb2d5f34c41e7d2ded
Taiwo AKerele <davcodes24@gmail.com>
2024-01-10 14:29:30 +0100

    successfully fetched the date

--- src/components/hook/context.jsx
+++ src/components/hook/context.jsx
@@ -1,1 +1,13 @@
 // https://api.edamam.com/search?q=Hamburger&app_id=5c336466&app_key=e1b6f5720747d4ef6c891759a5f5d0fa
+import React from 'react';
+
+const Context = () => {
+    
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Context;
--- src/components/pages/Food.jsx
+++ src/components/pages/Food.jsx
@@ -1,19 +1,47 @@
-import React, { useState } from "react";
-import searchIcon from '../images/searchIcon.svg'
+import React, { useEffect, useState } from "react";
+import searchIcon from "../images/searchIcon.svg";
 
 const Food = () => {
-    const [value, setValue] = useState('');
-    const handleChange = (e) => {
-        setValue(e.target.value);
+  const api_key = "e1b6f5720747d4ef6c891759a5f5d0fa";
+  const api_id = "5c336466";
+  const [value, setValue] = useState("");
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `https://api.edamam.com/search?q=${value}&app_id=${api_id}&app_key=${api_key}`
+      );
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+  
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error('Error fetching data:', error.message);
     }
+  };
+  
+
+  useEffect(() => {
+    if(value) {
+        fetchData()
+    }
+  },[value]);
+
+
   return (
     <div className="food">
       <h1>Search The recipe of your food</h1>
-      <img src={searchIcon} alt="searcher" />
+      <img src={searchIcon} onClick={fetchData} alt="searcher" />
 
-      <input type="search" id="search" placeholder="search recipe..." 
+      <input
+        type="search"
+        id="search"
+        placeholder="search recipe..."
         value={value}
-        onChange={handleChange}
+        onChange={(e) => setValue(e.target.value)}
+
       />
     </div>
   );
